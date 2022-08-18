cf137ad69cb357c12eb22feeb3af9f2c4ba32ba5
youngkyo0504 <keumky2@gmail.com>
2022-08-18 21:18:29 +0900

    feat: cors 설정
    - credential을 true로 해야 cookie값 저장할 수 있음
    
    - #6

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,8 +1,10 @@
+import axios from 'axios';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 
+axios.defaults.withCredentials = true;
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
     <Router>
