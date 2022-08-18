ae697f712b6ea08fa5396e3d7576206ab3c80250
youngkyo0504 <keumky2@gmail.com>
2022-08-18 17:44:08 +0900

    fix: react strict모드 제거
    - 의도치않은 랜더링과정을 제거

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -5,9 +5,7 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-  <React.StrictMode>
     <Router>
       <App />
     </Router>
-  </React.StrictMode>,
 );
