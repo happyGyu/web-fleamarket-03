fa139283e5e9d9733d5c9505adc45adcd6329ae6
happyGyu <happyboyto.dev@gmail.com>
2022-08-18 14:30:41 +0900

    feat: 로그인 로직 유틸함수로 분리
    - 재사용이 되지 않더라도 컴포넌트는 뷰와 상태에 집중하고 처리로직은 분리하기로 함
    
    - #5
    
    Co-authored-by: youngkyo0504 <keumky2@gmail.com>

--- client/src/constants/env.ts
+++ client/src/constants/env.ts
@@ -1,4 +1,4 @@
-export const baseUrl =
+export const BASE_URL =
   process.env.NODE_ENV === 'development' ? process.env.REACT_APP_CLIENT_DEV_URL : '';
 
-export const test = '';
+export const TEST = '';
