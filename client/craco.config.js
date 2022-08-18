40e76beac83758ed4371ab3ce65c41739592d5b7
happyGyu <happyboyto.dev@gmail.com>
2022-08-18 11:39:19 +0900

    style: 절대 경로 추가 설정
    - #5

--- client/craco.config.js
+++ client/craco.config.js
@@ -6,8 +6,10 @@ module.exports = {
   webpack: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
-
-      '@components': path.resolve(__dirname, 'src/components'),
+      '@constants': path.resolve(__dirname, 'src', 'constants'),
+      '@components': path.resolve(__dirname, 'src', 'components'),
+      '@pages': path.resolve(__dirname, 'src', 'pages'),
+      '@style': path.resolve(__dirname, 'src', 'style'),
     },
   },
 };
