9410bbd308416425a744d1eae96f7150d5721d3f
youngkyo0504 <keumky2@gmail.com>
2022-08-18 11:14:17 +0900

    refactor: region에 repository 추가 및 코드 단순화
    - repository 파일 적용
    - module에서 typeOrmModule 삭제
    
    - #2

--- server/src/region/region.module.ts
+++ server/src/region/region.module.ts
@@ -1,14 +1,12 @@
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
 import { RegionService } from './region.service';
 import { RegionController } from './region.controller';
-import { Region } from './entities/region.entity';
-import { UserRegion } from './entities/userRegion.entity';
+import { RegionRepository } from './repository/region.repository';
+import { UserRegionRepository } from './repository/userRegion.repository';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Region, UserRegion])],
   controllers: [RegionController],
-  providers: [RegionService],
+  providers: [RegionService, RegionRepository, UserRegionRepository],
   exports: [RegionService],
 })
 export class RegionModule {}
--- server/src/region/region.service.ts
+++ server/src/region/region.service.ts
@@ -1,37 +1,29 @@
 import { CreateUserRegionDto } from './dto/createUserRegion.dto';
-import { UserRegion } from 'src/region/entities/userRegion.entity';
 import { Injectable } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
-import { Like, Repository } from 'typeorm';
 import { CreateRegionDto } from './dto/createRegion.dto';
-import { Region } from './entities/region.entity';
+import { RegionRepository } from './repository/region.repository';
+import { UserRegionRepository } from './repository/userRegion.repository';
 
 @Injectable()
 export class RegionService {
   constructor(
-    @InjectRepository(Region)
-    private regionRepository: Repository<Region>,
-    @InjectRepository(UserRegion)
-    private userRegionRepository: Repository<UserRegion>,
+    private readonly userRegionRepository: UserRegionRepository,
+    private readonly regionRepository: RegionRepository, // private readonly orderItemRepository: OrderItemRepository,
   ) {}
 
   async create(createRegionDto: CreateRegionDto) {
-    const newRegion = this.regionRepository.create(createRegionDto);
-    await this.regionRepository.save(newRegion);
+    const newRegion = await this.regionRepository.create(createRegionDto);
     return newRegion.id;
   }
 
   async createUserRegion(createUserRegionDto: CreateUserRegionDto) {
-    const newUserRegion = this.userRegionRepository.create(createUserRegionDto);
-    await this.userRegionRepository.save(newUserRegion);
+    const newUserRegion = await this.userRegionRepository.create(
+      createUserRegionDto,
+    );
     return newUserRegion.id;
   }
 
   async searchByKeyword(keyword: string) {
-    return await this.regionRepository.find({
-      where: {
-        address: Like(`%${keyword}%`),
-      },
-    });
+    return this.regionRepository.findByKeyword(keyword);
   }
 }
