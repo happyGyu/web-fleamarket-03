faa6db57dafd8c47336ba5d4c2a65a9fc1c47ba1
happyGyu <happyboyto.dev@gmail.com>
2022-08-20 15:20:56 +0900

    feat: 공용 navigation bar 구현
    - 모든 페이지 상단에 위치하는 nav bar 구현
    - 기본으로는 왼쪽 버튼 클릭시 뒷 페이지로 이동
    - 커스텀 이벤트를 props로 받음
    - 오른쪽에 추가 버튼을 props로 전달 가능
    
    - #12

---
+++ client/src/components/common/NavigationBar.tsx
@@ -0,0 +1,63 @@
+import colors from '@constants/colors';
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
+import { Text } from '@components/common/Text';
+import ChevronLeft from '@assets/icons/ChevronLeft';
+
+interface NavigationBarProps {
+  title: string;
+  navigationButtonHandler?: () => void;
+  actionItem?: React.ReactNode;
+}
+
+export default function NavigationBar({
+  title,
+  navigationButtonHandler,
+  actionItem,
+}: NavigationBarProps) {
+  const navigate = useNavigate();
+
+  const defaultNavigationHandler = () => {
+    navigate(-1);
+  };
+  return (
+    <Contianer>
+      <NavigationButton onClick={navigationButtonHandler || defaultNavigationHandler}>
+        <ChevronLeft />
+      </NavigationButton>
+      <Text>{title}</Text>
+      <ActionItemContainer>{actionItem}</ActionItemContainer>
+    </Contianer>
+  );
+}
+
+const Contianer = styled.div`
+  position: absolute;
+  top: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 3.5rem;
+  padding: 1rem 1.5rem;
+  background-color: ${colors.offWhite};
+  border-bottom: 2px solid ${colors.grey1};
+`;
+
+const NavigationButton = styled.button`
+  position: absolute;
+  top: 1.25rem;
+  left: 1.5rem;
+
+  svg {
+    width: 0.5rem;
+    height: 1rem;
+  }
+`;
+
+const ActionItemContainer = styled.div`
+  position: absolute;
+  top: 1.25rem;
+  right: 1.5rem;
+`;
