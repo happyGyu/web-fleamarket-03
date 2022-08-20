18a637be5dbc97ee5103fb5fbd62c50ebd86daec
youngkyo0504 <keumky2@gmail.com>
2022-08-20 17:51:09 +0900

    feat: 전체 page Container에 대한 padding top
    - navigation bar만큼 띄어준다.
    
    - #14

---
+++ client/src/components/common/PageContainer.tsx
@@ -0,0 +1,8 @@
+import { padding } from '@constants/padding';
+import styled from 'styled-components';
+
+const PageContainer = styled.div`
+  padding: ${padding.pageTop} 0;
+`;
+
+export default PageContainer;
