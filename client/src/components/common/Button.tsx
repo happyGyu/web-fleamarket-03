ab9c7817218a14501703ec54385a65bdca6c75d4
happyGyu <happyboyto.dev@gmail.com>
2022-08-20 15:19:52 +0900

    feat: 공용 버튼 컴포넌트 디테일한 설정 추가 및 안쓰는 컴포넌트 삭제
    - 버튼 컴포넌트는 jsx를 반환할 필요없이 스타일드 컴포넌트면 충분
    - 따라서 이전에 만들었던 버튼 컴포넌트를 삭제
    
    - #12
    
    Co-authored-by: youngkyo0504 <keumky2@gmail.com>

--- client/src/components/common/Button.tsx
+++ client/src/components/common/Button.tsx
@@ -1,14 +1,15 @@
 import colors from '@constants/colors';
+import { fontWeight } from '@constants/fonts';
 import styled from 'styled-components';
 
 const buttonSizeMap = {
   medium: {
-    width: 111,
-    height: 111,
+    width: '8.5rem',
+    height: '2.25rem',
   },
   large: {
-    width: 111,
-    height: 111,
+    width: '18.125rem',
+    height: '2.625rem',
   },
 };
 
@@ -17,8 +18,10 @@ type ButtonSizeType = 'medium' | 'large';
 const Button = styled.button<{ size: ButtonSizeType }>`
   width: ${({ size }) => buttonSizeMap[size].width};
   height: ${({ size }) => buttonSizeMap[size].height};
+  font-weight: ${fontWeight.link};
   color: ${colors.offWhite};
   background-color: ${colors.primary};
+  border-radius: 0.5rem;
 
   :hover {
     background-color: ${colors.tertiary};
