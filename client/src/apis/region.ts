2296647e70cbac1c6d18cddf979d90976dac2bf7
happyGyu <happyboyto.dev@gmail.com>
2022-08-20 17:51:39 +0900

    feat: api 폴더 생성 및 region 검색 api 함수 추가
    - #15

---
+++ client/src/apis/region.ts
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+export async function searchRegionByKeyword(keyword: string) {
+  if (keyword === '') return [];
+  try {
+    const { data } = await axios.get(`/region/search?keyword=${keyword}`);
+    return data.regions;
+  } catch (e) {
+    throw new Error('검색에 실패했습니다.');
+  }
+}
