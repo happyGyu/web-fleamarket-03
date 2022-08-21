cc3881af239a085a180be28402b130c11e52b5f5
youngkyo0504 <keumky2@gmail.com>
2022-08-22 00:58:17 +0900

    feat: post 마크업 추가
    - #31

--- client/src/pages/Routes.tsx
+++ client/src/pages/Routes.tsx
@@ -3,6 +3,7 @@ import ErrorPage from './ErrorPage';
 import LoginPage from './LoginPage';
 import MainPage from './MainPage';
 import OAuthRedirectPage from './OAuthRedirectPage';
+import PostPage from './PostPage';
 import SignUpPage from './SignUpPage';
 
 export default function Routes() {
@@ -14,6 +15,7 @@ export default function Routes() {
       <Route path="/oauth-redirect" element={<OAuthRedirectPage />} />
       <Route path="/signUp" element={<SignUpPage />} />
       <Route path="/error" element={<ErrorPage />} />
+      <Route path="/post" element={<PostPage />} />
       <Route path="/" element={isLogin ? <MainPage /> : <Navigate to="/login" />} />
     </RouterRoutes>
   );
