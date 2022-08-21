b8002c0bf2d8482fc5280702f35a75733e7015ed
youngkyo0504 <keumky2@gmail.com>
2022-08-21 22:54:00 +0900

    feat: ImageUploader 마크업 작성
    - 데이터 패칭 등 로직은 작성 x
    
    - #31

---
+++ client/src/components/Post/CircleXButton.tsx
@@ -0,0 +1,38 @@
+import XIcon from '@assets/icons/XIcon';
+import colors from '@constants/colors';
+import mixin from '@style/mixin';
+import styled from 'styled-components';
+
+export default function CircleXButton() {
+  return (
+    <CircleButtonWrapper>
+      <XIcon />
+    </CircleButtonWrapper>
+  );
+}
+
+const CircleButtonWrapper = styled.button`
+  width: 1.5rem;
+  height: 1.5rem;
+  background-color: ${colors.black};
+  border-radius: 50%;
+  ${mixin.flexMixin({ justify: 'center', align: 'center' })}
+
+  :hover {
+    background-color: ${colors.grey1};
+  }
+
+  :disabled {
+    background-color: ${colors.secondary};
+  }
+
+  :focus {
+    border: 1px solid ${colors.tertiary};
+  }
+
+  svg {
+    stroke: ${colors.white};
+    width: 1rem;
+    height: 1rem;
+  }
+`;
