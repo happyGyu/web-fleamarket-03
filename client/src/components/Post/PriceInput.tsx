9b2f49a504d8651bd896c1e4b032e8da90f2ced6
youngkyo0504 <keumky2@gmail.com>
2022-08-21 23:43:42 +0900

    feat: price input 마크업 작성
    - #31

--- client/src/components/Post/PriceInput.tsx
+++ client/src/components/Post/PriceInput.tsx
@@ -2,11 +2,11 @@ import colors from '@constants/colors';
 import { fontSize } from '@constants/fonts';
 import styled from 'styled-components';
 
-export default function TitleInput() {
+export default function PriceInput() {
   return (
     <>
       <Container>
-        <CustomInput placeholder="글 제목" />
+        <CustomInput placeholder="글 제목" value="₩ 169,000" />
       </Container>
       <div />
     </>
@@ -14,12 +14,14 @@ export default function TitleInput() {
 }
 
 const Container = styled.div`
-  padding: 24px 0;
+  padding: 24px 0 18px 0;
+  border-bottom: 1px solid ${colors.grey3};
 `;
 
 const CustomInput = styled.input`
   width: 100%;
   color: ${colors.black};
+
   background-color: ${colors.white};
   border: 0;
   font-size: ${fontSize.large};
