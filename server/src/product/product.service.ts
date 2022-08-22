b2aeb179984005d9ecd45a98df92c5132bb5dbb2
youngkyo0504 <keumky2@gmail.com>
2022-08-23 01:08:43 +0900

    feat: product service에 update, create 메소드 작성
    - #37

--- server/src/product/product.service.ts
+++ server/src/product/product.service.ts
@@ -1,6 +1,8 @@
 import { ProductRepository } from './repository/product.repository';
 import { Injectable } from '@nestjs/common';
 import { Product } from './entities/product.entity';
+import { CreateProductDto } from './dto/createProduct.dto';
+import { UpdateProductDto } from './dto/updateProductDto';
 
 @Injectable()
 export class ProductService {
@@ -8,5 +10,21 @@ export class ProductService {
 
   getRegionProducts(regionId: number): Promise<Product[]> {
     return this.productRepository.findProductsByRegion(regionId);
+  }
+
+  createNewProduct(createProductDto: CreateProductDto) {
+    return this.productRepository.createProduct(createProductDto);
+  }
+
+  updateProductById(
+    id: number,
+    sellerId: number,
+    updateProductDto: UpdateProductDto,
+  ) {
+    return this.productRepository.patchProductById(
+      id,
+      sellerId,
+      updateProductDto,
+    );
   }
 }
