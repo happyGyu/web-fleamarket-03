55371c3d441d609951c7de71b36515a895aa280c
happyGyu <happyboyto.dev@gmail.com>
2022-08-22 22:33:07 +0900

    feat: 메인페이지 헤더도 실제 유저 데이터를 바탕으로 그리도록 수정
    - #34

--- client/src/components/MainPageNavigationBar/MainNavTitle.tsx
+++ client/src/components/MainPageNavigationBar/MainNavTitle.tsx
@@ -1,15 +1,22 @@
+import { getUser } from '@apis/user';
 import MapPinIcon from '@assets/icons/MapPinIcon';
+import LoadingIndicator from '@components/common/LoadingIndicator';
 import { Text } from '@components/common/Text';
 import colors from '@constants/colors';
 import mixin from '@style/mixin';
+import { useQuery } from '@tanstack/react-query';
+import { getLastAddress } from '@utils/product';
 import styled from 'styled-components';
 
 export default function MainNavTitle() {
-  return (
+  const { data: user } = useQuery(['user'], getUser);
+  return user ? (
     <Container>
       <MapPinIcon />
-      <Text>역삼동</Text>
+      <Text>{getLastAddress(user.regions[0].region.address)}</Text>
     </Container>
+  ) : (
+    <LoadingIndicator />
   );
 }
 
