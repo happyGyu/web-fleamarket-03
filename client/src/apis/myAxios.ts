622fb5fe4bca0f55733f35e896a49e3391668869
happyGyu <happyboyto.dev@gmail.com>
2022-08-22 21:40:13 +0900

    feat: axios interceptor를 적용한 myaxios 인스턴스 구현
    - 토큰이 없어서 요청에 실패하면 인터셉터에서 리프레시를 시도하고 기존 요청을 재시도
    
    - #34

---
+++ client/src/apis/myAxios.ts
@@ -0,0 +1,32 @@
+import { SERVER_URL } from '@constants/env';
+import axios from 'axios';
+
+const myAxios = axios.create({
+  withCredentials: true,
+  baseURL: SERVER_URL,
+});
+
+myAxios.interceptors.response.use(
+  (response) => response,
+
+  async (error) => {
+    const {
+      response: { status },
+      config: originalRequest,
+    } = error;
+
+    if (status === 401) {
+      await reissueTokenAndRetryRequest(originalRequest);
+    }
+
+    return Promise.reject(error);
+  },
+);
+
+async function reissueTokenAndRetryRequest(originalRequest: any) {
+  const { data: accessToken } = await axios.get('/auth/resign');
+  myAxios.defaults.headers.common.Authorization = `Bearer ${accessToken}`;
+  return myAxios(originalRequest);
+}
+
+export default myAxios;
