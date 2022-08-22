6ffb1c98d3148d5e0e2819242149f4fd4089a283
happyGyu <happyboyto.dev@gmail.com>
2022-08-23 02:31:48 +0900

    feat: 드롭다운을 활용한 상품 유틸버튼 구현
    - 수정하기, 삭제하기가 옵션으로 주어짐
    
    - #28

---
+++ client/src/components/MoreButton/index.tsx
@@ -0,0 +1,38 @@
+import MoreIcon from '@assets/icons/MoreIcon';
+import DropDown from '@components/common/DropDown';
+import colors from '@constants/colors';
+import { useState } from 'react';
+import styled from 'styled-components';
+
+interface MoreButtonProps {
+  color?: string;
+}
+
+export default function MoreButton({ color }: MoreButtonProps) {
+  const [isDropDownOpen, setIsDropDownOpen] = useState(false);
+  const moreUtils = [
+    {
+      name: '수정하기',
+      onClick: () => alert('수정하기'),
+    },
+    {
+      name: '삭제하기',
+      onClick: () => alert('삭제하기'),
+      color: colors.red,
+    },
+  ];
+  return (
+    <Container color={color} onClick={() => setIsDropDownOpen((prev) => !prev)}>
+      <MoreIcon />
+      {isDropDownOpen && <DropDown dropDownItems={moreUtils} top="2rem" right="0" />}
+    </Container>
+  );
+}
+
+const Container = styled.button<{ color?: string }>`
+  svg {
+    width: 1.5rem;
+    height: 1.5rem;
+    stroke: ${({ color }) => color};
+  }
+`;
