f85996bc23ef8d5f3d4ff88b57d8eb79cf0cb201
youngkyo0504 <keumky2@gmail.com>
2022-08-25 00:33:01 +0900

    feat: post 수정 구현
    - #45

--- client/src/pages/ProductEditPage.tsx
+++ client/src/pages/ProductEditPage.tsx
@@ -1,4 +1,4 @@
-import { createProduct } from '@apis/product';
+import { getProductDetail, updateProduct } from '@apis/product';
 import { getUser } from '@apis/user';
 import NavigationBar from '@components/common/NavigationBar';
 import PageContainer from '@components/common/PageContainer';
@@ -15,20 +15,23 @@ import { CreateProductAPIDto } from '@customTypes/product';
 import { useQuery } from '@tanstack/react-query';
 import { getNumber } from '@utils/format';
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import styled from 'styled-components';
 
-export default function PostPage() {
+export default function ProductEditPage() {
   return (
     <FormProvider>
-      <PostForm />
+      <PostEditForm />
     </FormProvider>
   );
 }
 
-function PostForm() {
+function PostEditForm() {
   const { data: user } = useQuery(['user'], getUser);
-
+  const { productId } = useParams();
+  const { data: product } = useQuery(['product', productId], () =>
+    getProductDetail(Number(productId)),
+  );
   const { formInputMap } = useFormInputMap();
   const { isAllValidated } = useFormValidationState();
 
@@ -41,25 +44,30 @@ function PostForm() {
       return;
     }
 
-    const product = {
+    const editedProduct = {
       ...formInputMap,
       price: Number(getNumber(formInputMap.price)),
+      // region primary구현해야함
       regionId: user?.regions[0].regionId,
     } as CreateProductAPIDto;
 
     try {
-      await createProduct(product);
+      await updateProduct(editedProduct, Number(productId));
       navigate('/');
     } catch (error) {
       navigate('error');
     }
   };
 
+  if (!product) {
+    return <div />;
+  }
+
   return (
     <Form onSubmit={registerProduct}>
       <NavigationBar title="글쓰기" actionItem={<SubmitButton />} />
       <PostPageWrapper>
-        <Post />
+        <Post product={product} />
         <RegionFooter />
       </PostPageWrapper>
     </Form>
