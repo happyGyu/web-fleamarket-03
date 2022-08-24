02574dba6cbbe0be0f5c231d9ec48599a1af0da6
youngkyo0504 <keumky2@gmail.com>
2022-08-24 10:44:46 +0900

    feat: titleinput validation , input 상태 추가

--- client/src/components/Post/TitleInput.tsx
+++ client/src/components/Post/TitleInput.tsx
@@ -1,17 +1,36 @@
 import colors from '@constants/colors';
 import { fontSize } from '@constants/fonts';
 import styled from 'styled-components';
+import React from 'react';
+import { ValidateMessage } from '@components/common/ValidationMessage';
+import { useForm } from '@components/CustomForm/useForm';
 
 export default function TitleInput() {
+  const validator = {
+    min: {
+      validate: (value: string) => value.length > 0,
+      errorMessage: '값이 비어있습니다.',
+    },
+  };
+
+  const { setInputValue, inputValue, validate, errorMessage } = useForm('title', '', validator);
+
+  const inputHandler = ({ currentTarget }: React.FormEvent<HTMLInputElement>) => {
+    const { value } = currentTarget;
+    validate({ value, validateProperties: ['min'] });
+    setInputValue(value);
+  };
+
   return (
     <Container>
-      <CustomInput placeholder="글 제목" />
+      <CustomInput placeholder="글 제목" value={inputValue} onInput={inputHandler} />
+      <ValidateMessage as="p">{errorMessage}</ValidateMessage>
     </Container>
   );
 }
 
 const Container = styled.div`
-  padding: 24px 0;
+  padding: 24px 0 0 0;
 `;
 
 const CustomInput = styled.input`
