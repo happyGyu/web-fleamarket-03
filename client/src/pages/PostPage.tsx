194c62c76fc83e3431a17cf19ac3db73c8b5e8bc
youngkyo0504 <keumky2@gmail.com>
2022-08-24 10:45:43 +0900

    feat: post page valiation 완료
    - #37

--- client/src/pages/PostPage.tsx
+++ client/src/pages/PostPage.tsx
@@ -1,22 +1,39 @@
-import CheckIcon from '@assets/icons/CheckIcon';
 import NavigationBar from '@components/common/NavigationBar';
 import PageContainer from '@components/common/PageContainer';
+import {
+  FormProvider,
+  useFormInputMap,
+  useFormValidationState,
+  useFormValidatorMapAction,
+} from '@components/CustomForm/FormProvider';
 import Post from '@components/Post';
 import RegionFooter from '@components/Post/RegionFooter';
+import { SubmitButton } from '@components/Post/SubmitButton';
 import { padding } from '@constants/padding';
+import React from 'react';
 import styled from 'styled-components';
 
 export default function PostPage() {
   return (
-    <>
-      <NavigationBar title="글쓰기" actionItem={<CheckIcon />} />
+    <FormProvider>
+      <PostForm />
+    </FormProvider>
+  );
+}
+
+function PostForm() {
+  const { formInputMap } = useFormInputMap();
+  const { isAllValidated } = useFormValidationState();
+  const { formValidatorMapAction } = useFormValidatorMapAction();
+
+  return (
+    <Form>
+      <NavigationBar title="글쓰기" actionItem={<SubmitButton />} />
       <PostPageWrapper>
-        <form>
-          <Post />
-        </form>
+        <Post />
         <RegionFooter />
       </PostPageWrapper>
-    </>
+    </Form>
   );
 }
 
@@ -27,4 +44,8 @@ const PostPageWrapper = styled(PageContainer)`
   width: 100%;
   height: 100%;
   background-color: white;
+`;
+
+const Form = styled.form`
+  height: 100%;
 `;
