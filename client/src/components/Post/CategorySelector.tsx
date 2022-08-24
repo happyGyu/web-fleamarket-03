2354f8028f14dcb4c9b6ca1c3f9222c2bae33e6a
youngkyo0504 <keumky2@gmail.com>
2022-08-24 10:44:03 +0900

    feat: category selector 상태 및 validation 추가
    - #37

--- client/src/components/Post/CategorySelector.tsx
+++ client/src/components/Post/CategorySelector.tsx
@@ -1,4 +1,5 @@
 import { Text } from '@components/common/Text';
+import { useForm } from '@components/CustomForm/useForm';
 import colors from '@constants/colors';
 import mixin from '@style/mixin';
 import styled from 'styled-components';
@@ -6,12 +7,38 @@ import styled from 'styled-components';
 export default function CategorySelector() {
   const categories = ['여성패션 잡화', '기타 물품', '가구 인테리어', '기타', 'it물품'];
 
+  const validator = {
+    exist: {
+      errorMessage: '카테고리를 선택해주세요',
+      validate: (value: null | number) => value !== null,
+    },
+  };
+
+  const {
+    inputValue: selectedCategoryId,
+    setInputValue: setSelectedCategoryId,
+    errorMessage,
+    validate,
+  } = useForm('category', 0, validator, { isInitialValid: true });
+
+  const selectCategory = (categoryId: number) => () => {
+    validate({ value: categoryId, validateProperties: ['exist'] });
+    setSelectedCategoryId(categoryId);
+  };
+
   return (
     <Container>
       <Text size="xSmall">(필수) 카테고리를 선택해주세요</Text>
+      <ErrorMessage size="xSmall" color="red">
+        {errorMessage}
+      </ErrorMessage>
       <CategoriesContainer>
-        {categories.map((category) => (
-          <ItemWrapper isActive={category === '여성패션 잡화'}>
+        {categories.map((category, index) => (
+          <ItemWrapper
+            key={category}
+            isActive={index === selectedCategoryId}
+            onClick={selectCategory(index)}
+          >
             <Text>{category}</Text>
           </ItemWrapper>
         ))}
@@ -20,13 +47,16 @@ export default function CategorySelector() {
   );
 }
 
+const ErrorMessage = styled(Text)`
+  color: ${colors.red};
+`;
+
 const Container = styled.div`
   padding-bottom: 24px;
 
   border-bottom: 1px solid ${colors.grey3};
   color: ${colors.grey1};
   width: 100%;
-  overflow-x: hidden;
   ${Text} {
     color: ${colors.grey2};
   }
@@ -44,6 +74,8 @@ const ItemWrapper = styled.div<{ isActive?: boolean }>`
   }
 `;
 const CategoriesContainer = styled.div`
+  overflow-x: auto;
+
   margin-top: 0.8rem;
   ${mixin.flexMixin({ align: 'center' })};
   gap: 4px;
