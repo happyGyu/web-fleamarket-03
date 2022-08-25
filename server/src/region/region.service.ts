4753d1459fb8cd829953829798ba8b7e422be11c
youngkyo0504 <keumky2@gmail.com>
2022-08-26 00:53:58 +0900

    feat: userRegion patch,post API 작성 완료
    - 순환참조 문제 useRef로 해결

--- server/src/region/region.service.ts
+++ server/src/region/region.service.ts
@@ -1,5 +1,5 @@
 import { CreateUserRegionDto } from './dto/createUserRegion.dto';
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { CreateRegionDto } from './dto/createRegion.dto';
 import { RegionRepository } from './repository/region.repository';
 import { UserRegionRepository } from './repository/userRegion.repository';
@@ -18,6 +18,35 @@ export class RegionService {
 
   async createUserRegion(createUserRegionDto: CreateUserRegionDto) {
     return await this.userRegionRepository.create(createUserRegionDto);
+  }
+
+  async setUserRegionPrimary(userId: number, regionId: number) {
+    const userRegions = await this.userRegionRepository.findByUserId(userId);
+    const targetUserRegion = userRegions.find(
+      (userRegion) => userRegion.regionId === regionId,
+    );
+
+    if (!targetUserRegion) {
+      throw new HttpException('지역이 올바르지 않습니다', HttpStatus.NOT_FOUND);
+    }
+
+    const { regionId: previousPrimaryRegionId } = userRegions.find(
+      (userRegion) => userRegion.regionId !== regionId,
+    );
+
+    if (previousPrimaryRegionId) {
+      this.userRegionRepository.updateIsPrimary(
+        userId,
+        previousPrimaryRegionId,
+        false,
+      );
+    }
+
+    return await this.userRegionRepository.updateIsPrimary(
+      userId,
+      regionId,
+      true,
+    );
   }
 
   async searchByKeyword(keyword: string) {
