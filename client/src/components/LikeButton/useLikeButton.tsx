7e1ad531f32348bb2c309064e19c157af337d6c1
happyGyu <happyboyto.dev@gmail.com>
2022-08-25 16:29:27 +0900

    chore: 좋아요 토글 로직 개선 투두 적어둠
    - #43

--- client/src/components/LikeButton/useLikeButton.tsx
+++ client/src/components/LikeButton/useLikeButton.tsx
@@ -26,6 +26,16 @@ export default function useLikeButton({
       const snapshot = queryClient.getQueryData<InfiniteData<GetRegionProductDto>>(queryKey);
       queryClient.setQueryData<InfiniteData<GetRegionProductDto>>(queryKey, (infinitePages) => {
         if (!infinitePages) return infinitePages;
+        // const products = infinitePages.pages.map((page) => page.products).flat();
+        // const targetProductIdx = products.findIndex((product) => product.id === productId);
+        // const targetProduct = products[targetProductIdx];
+        // if (!targetProduct) throw new Error('상품이 존재하지 않습니다.');
+        // const isAlreadyLiked = targetProduct.likedUsers.find(({ userId }) => userId === user.id);
+        // const toggledProduct = { ...targetProduct };
+        // toggledProduct.likedUsers.push({ userId: user.id, productId });
+        // products[targetProductIdx] = toggledProduct;
+
+        // todo: 개선필요
         const a = infinitePages.pages.map((productPage) => ({
           ...productPage,
           products: productPage.products.map((product) => {
