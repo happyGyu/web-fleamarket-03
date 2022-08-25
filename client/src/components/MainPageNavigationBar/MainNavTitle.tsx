3efbc13e9ac9c8426a406d04189ca13aceaeb846
youngkyo0504 <keumky2@gmail.com>
2022-08-26 01:09:08 +0900

    feat: region select Dropdown 작성

--- client/src/components/MainPageNavigationBar/MainNavTitle.tsx
+++ client/src/components/MainPageNavigationBar/MainNavTitle.tsx
@@ -1,19 +1,48 @@
 import { getUser } from '@apis/user';
 import MapPinIcon from '@assets/icons/MapPinIcon';
+import DropDown from '@components/common/DropDown';
 import LoadingIndicator from '@components/common/LoadingIndicator';
 import { Text } from '@components/common/Text';
 import colors from '@constants/colors';
 import mixin from '@style/mixin';
 import { useQuery } from '@tanstack/react-query';
 import { getLastAddress } from '@utils/product';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 
 export default function MainNavTitle() {
   const { data: user } = useQuery(['user'], getUser);
+
+  const [isDropDownOpen, setIsDropDownOpen] = useState(false);
+  const navigate = useNavigate();
+
+  const regions = user?.regions || [];
+
+  const dropdownItems = [
+    ...regions.map(({ region }) => ({
+      onClick: () => alert('primary region 를 바꿉니다.'),
+      name: getLastAddress(region.address) || '',
+    })),
+    {
+      name: '내 동네 설정하기',
+      onClick: () => navigate('/user/region'),
+    },
+  ];
+
   return user ? (
-    <Container>
+    <Container
+      onClick={(e) => {
+        e.stopPropagation();
+        setIsDropDownOpen((prev) => !prev);
+      }}
+    >
       <MapPinIcon />
-      <Text>{getLastAddress(user.regions[0].region.address)}</Text>
+      {/* // ! TODO 여기 region이 프라이머리 region이 아님    */}
+      <Text color={colors.white}>{getLastAddress(user.regions[0].region.address)}</Text>
+      {isDropDownOpen && (
+        <DropDown dropDownItems={dropdownItems} top="85%" transform="translate(-25%,0)" />
+      )}
     </Container>
   ) : (
     <LoadingIndicator />
@@ -23,9 +52,7 @@ export default function MainNavTitle() {
 const Container = styled.button`
   ${mixin.flexMixin({ align: 'center' })}
   gap: 0.25rem;
-  ${Text} {
-    color: ${colors.white};
-  }
+
   svg {
     scale: 0.9;
     stroke: ${colors.white};
