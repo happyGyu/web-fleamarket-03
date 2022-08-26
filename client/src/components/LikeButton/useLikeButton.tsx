96b10cc7bde622bba79d81d7bf8e87143dc5eb1e
happyGyu <happyboyto.dev@gmail.com>
2022-08-26 13:09:00 +0900

    chore: 이전 커밋 누락분

--- client/src/components/LikeButton/useLikeButton.tsx
+++ client/src/components/LikeButton/useLikeButton.tsx
@@ -1,6 +1,7 @@
 import { toggleLike } from '@apis/product';
 import { IProductItem } from '@customTypes/product';
 import { useProduct } from '@queries/useProduct';
+import { useUser } from '@queries/useUser';
 import { UseMutateFunction, useMutation, useQueryClient } from '@tanstack/react-query';
 
 interface UseLikeButtonprops {
@@ -10,22 +11,53 @@ interface UseLikeButtonprops {
 export default function useLikeButton({
   productId,
 }: UseLikeButtonprops): UseMutateFunction<void, unknown, void, unknown> {
+  const queryKey = ['product', productId];
   const queryClient = useQueryClient();
+  const user = useUser();
   const { refetch } = useProduct(productId);
   const { mutate } = useMutation(() => toggleLike(productId), {
     onMutate: () => {
-      const snapshot = queryClient.getQueryData<IProductItem>(['prduct', productId]);
+      const snapshot = queryClient.getQueryData<IProductItem>(queryKey);
+      const optimisticUpdated = optimisticUpdator({
+        original: snapshot,
+        userId: user.id,
+        productId,
+      });
+      queryClient.setQueryData(queryKey, optimisticUpdated);
       return { snapshot };
     },
 
     onError: (error, variables, context) => {
-      queryClient.setQueryData(['product', productId], context?.snapshot);
-    },
-
-    onSuccess: () => {
-      refetch();
+      queryClient.setQueryData(queryKey, context?.snapshot);
     },
   });
 
   return mutate;
 }
+
+interface OptimisticUpdatorProps {
+  original?: IProductItem;
+  userId: number;
+  productId: number;
+}
+
+const optimisticUpdator = ({
+  original,
+  userId,
+  productId,
+}: OptimisticUpdatorProps): IProductItem | undefined => {
+  if (!original) return undefined;
+
+  const newLikedUsers = [...original.likedUsers];
+  const likedLogIndex = original.likedUsers.findIndex((likedUser) => likedUser.userId === userId);
+  if (likedLogIndex === -1) {
+    newLikedUsers.push({
+      userId,
+      productId,
+    });
+  } else {
+    newLikedUsers.splice(likedLogIndex, 1);
+  }
+
+  return { ...original, likedUsers: newLikedUsers };
+};
