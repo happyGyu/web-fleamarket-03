b2cb0ca556c9b66332828bf1844051fae1ca3c2c
youngkyo0504 <keumky2@gmail.com>
2022-08-27 16:10:10 +0900

    fix: eslint 파일 대소문자 fix

--- client/src/components/common/toast/index.tsx
+++ client/src/components/common/toast/index.tsx
@@ -1,6 +1,6 @@
 import colors from '@constants/colors';
 import styled, { css } from 'styled-components';
-import { ToastType, useToastState } from './toastContext';
+import { useToastState, ToastType } from './toastContext';
 
 export default function Toast() {
   const { isOpened, message } = useToastState();
