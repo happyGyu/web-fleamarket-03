cf34fbeaaf7dc69b173605f51b7865b2b929d2f6
happyGyu <happyboyto.dev@gmail.com>
2022-08-27 15:31:02 +0900

    feat: 로그아웃 페이지 사진 추가 및 레이아웃 수정

--- client/src/pages/LogOutPage.tsx
+++ client/src/pages/LogOutPage.tsx
@@ -2,6 +2,8 @@ import NavigationBar from '@components/common/NavigationBar';
 import PageContainer from '@components/common/PageContainer';
 import LogOutButton from '@components/LogOutPage/LogOutButton';
 import { useUser } from '@queries/useUser';
+import mixin from '@style/mixin';
+import styled from 'styled-components';
 
 export default function LogOutPage() {
   const { user } = useUser();
@@ -9,10 +11,22 @@ export default function LogOutPage() {
   return (
     <>
       <NavigationBar shadowColor="transparent" title="로그아웃" />
-      <PageContainer>
-        <h2>{user.name}님, 황금마켓에서의 시간은 즐거우셨나요?</h2>
+      <LogoutPageContainer>
+        <LogoutPageImage src="/sadBadal.png" alt="슬픈배달이" />
+        <h2>{`"${user.name}"`} 님, 벌써 가시나요?</h2>
         <LogOutButton />
-      </PageContainer>
+      </LogoutPageContainer>
     </>
   );
 }
+
+const LogoutPageContainer = styled(PageContainer)`
+  margin-top: 3rem;
+  ${mixin.flexMixin({ direction: 'column', align: 'center' })}
+  gap: 2rem;
+`;
+
+const LogoutPageImage = styled.img`
+  width: 10rem;
+  height: 10rem;
+`;
