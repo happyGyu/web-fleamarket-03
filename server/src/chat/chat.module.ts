ab8f73d1a7070429be2b971da3a76d66a669a967
happyGyu <happyboyto.dev@gmail.com>
2022-08-28 14:05:21 +0900

    feat: chat module 추가

---
+++ server/src/chat/chat.module.ts
@@ -0,0 +1,21 @@
+import { ProductRepository } from './../product/repository/product.repository';
+import { UserRepository } from './../user/repository/user.repository';
+import { ChatMessageRepository } from './repository/chatMessage.repository';
+import { ChatRoomRepository } from './repository/chatRoom.repository';
+import { AuthenticationModule } from 'src/authentication/authentication.module';
+import { Module } from '@nestjs/common';
+import { ChatController } from './chat.controller';
+import { ChatService } from './chat.service';
+
+@Module({
+  imports: [AuthenticationModule],
+  controllers: [ChatController],
+  providers: [
+    ChatService,
+    ChatRoomRepository,
+    ChatMessageRepository,
+    UserRepository,
+    ProductRepository,
+  ],
+})
+export class ChatModule {}
