c06c59dd0d687dcac8e413be407bbad0b83b2a6d
happyGyu <happyboyto.dev@gmail.com>
2022-08-28 19:51:07 +0900

    feat: 서버에서 웹소켓을 사용하는 gateWay 생성
    - #78

---
+++ server/src/chat/chat.gateway.ts
@@ -0,0 +1,74 @@
+import {
+  ConnectedSocket,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+  WsException,
+} from '@nestjs/websockets';
+import { ChatService } from './chat.service';
+import { Server, Socket } from 'socket.io';
+
+interface EnterChatRoomProps {
+  userId: string;
+  chatRoomId: string;
+}
+
+interface SendChatProps {
+  senderId: number;
+  content: string;
+}
+
+@WebSocketGateway(8080, {
+  namespace: 'goldMarket',
+  cors: true,
+})
+export class ChatGateWay implements OnGatewayConnection, OnGatewayDisconnect {
+  constructor(private readonly chatService: ChatService) {}
+
+  @WebSocketServer()
+  server: Server;
+
+  chatRooms = {};
+
+  handleConnection(@ConnectedSocket() client: Socket) {
+    console.log(`${client.id}연결됐어요!`);
+    client.leave(client.id);
+  }
+
+  handleDisconnect(client: Socket) {
+    console.log(`${client.id}끊어졌어요!`);
+    client.leave(client.data.chatRoomId);
+  }
+
+  @SubscribeMessage('enter')
+  async enterRoom(client: Socket, { userId, chatRoomId }: EnterChatRoomProps) {
+    try {
+      const chatRoom = await this.chatService.getChatRoom(+chatRoomId);
+      if (!chatRoom) throw Error('존재하지 않는 채팅방입니다.');
+      if (chatRoom.buyerId !== +userId && chatRoom.sellerId !== +userId) {
+        throw Error('당신의 채팅방이 아닙니다.');
+      }
+      this.chatRooms[chatRoomId] = { userId, chatRoomId };
+      //client.data.userId = userId;
+      client.data.chatRoomId = chatRoomId;
+      client.join(String(chatRoomId));
+      console.log(`${chatRoomId}에 ${userId}가 들어왔어요`);
+    } catch (e) {
+      console.log(e);
+      throw new WsException(e.message || '서버에 문제가 있습니다.');
+    }
+  }
+
+  @SubscribeMessage('send')
+  async sendMessage(client: Socket, { content, senderId }: SendChatProps) {
+    const { chatRoomId } = client.data;
+    const chatMessage = await this.chatService.createChatMessage({
+      content,
+      senderId,
+      chatRoomId: +chatRoomId,
+    });
+    client.to(String(chatRoomId)).emit('receive', chatMessage);
+  }
+}
