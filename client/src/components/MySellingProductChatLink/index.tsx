f24d6e8937a6814cb4840c1f002feaae82c4449b
happyGyu <happyboyto.dev@gmail.com>
2022-08-30 13:53:38 +0900

    fix: 현재동네에 상품이 추가되지 않는 문제 수정

--- client/src/components/MySellingProductChatLink/index.tsx
+++ client/src/components/MySellingProductChatLink/index.tsx
@@ -8,7 +8,6 @@ interface ChatButtonProps {
 }
 
 export default function ChatButton({ chattingRooms, productId }: ChatButtonProps) {
-  const numberOfChattingRooms = chattingRooms.length;
   const navigate = useNavigate();
 
   const onClick = () => {
@@ -17,7 +16,7 @@ export default function ChatButton({ chattingRooms, productId }: ChatButtonProps
 
   return (
     <Button onClick={onClick} size="medium">
-      채팅 목록 보기 {numberOfChattingRooms ? `(${numberOfChattingRooms})` : ''}{' '}
+      채팅 목록 보기
     </Button>
   );
 }
